fce92b6fc23bc8a861582698c473933a2997ef81
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-07-01 15:42:49 +0530

    Demo app in Django

---
+++ mysite/polls/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+
+
+urlpatterns = [
+    url('', views.index, name='index'),
+    url('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url('<int:question_id>/vote/', views.vote, name='vote'),
+]
